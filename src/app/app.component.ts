39369ac3089f5c28b78c66d6f9e228e9772a423a
Juan Pablo Duran <pablo_duran@unicomer.com>
2025-01-10 17:19:45 -0300

    added lasttest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgxChileanPpuService, PpuValidation } from 'ngx-chilean-ppu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+imports: [
+  FormsModule,
+  ReactiveFormsModule
+],
+  template: `
+  <div class="container">
+    <div class="row">
+    <h1>PPU Tester</h1>
+      <div class="col-md-12">
+        <form [formGroup]="formulario" class="form-horizontal">
+          <input class="form-control" formControlName="ppu" name="ppu" type="text" placeholder="" />
+        </form>
+
+         @if(formulario.get('ppu')!.invalid){
+            @if(formulario.get('ppu')!.errors?.['required']){
+              <p style="color: #ff0000;">
+              <span>El campo es requerido</span>
+              </p>
+            }
+            @if(formulario.get('ppu')!.errors?.['ppuInvalid']){
+              <p style="color: #ff0000;">
+              <span >El placa patente no es válida</span>
+              </p>
+            }
+        }
+
+        </div>
+        <div class="col-md-12">
+          @if(formulario.get('ppu')!.valid){
+            <p> PPU: {{ formulario.get('ppu')!.value }}</p>
+            <p> DV ppu: {{ ppuService.calculateDv(formulario.get('ppu')!.value ) }}</p>
+          }
+
+        </div>
+    </div>
+
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'example-ngx-chilean-ppu';
+  public formulario: FormGroup;
+  public ppu = 'XL1187';
+  public ppuService = inject(NgxChileanPpuService);
+  public ppuValidation = inject(PpuValidation);
+  constructor(private formBuilder: FormBuilder) {
+    this.formulario = this.formBuilder.group({
+      ppu: ['', [Validators.required, this.ppuValidation.ppuValid()]],
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePpu } from 'ngx-chilean-ppu';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+        providePpu()
+
+
+  ]
 };
